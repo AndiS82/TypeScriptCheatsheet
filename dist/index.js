51836c214d5a0067183edb5357d319ecc5ad1869
Andreas Schneider <xichtus@MacBook-Pro-von-Andreas.fritz.box>
2023-04-28 16:36:55 +0200

    progress

--- dist/index.js
+++ dist/index.js
@@ -8,4 +8,47 @@ let sales2 = 321;
 let course = 'TypeScript';
 let is_published = true;
 let affe;
+function render(document) {
+    console.log(document);
+}
+let numbers = [1, 2, '3'];
+let trueNumbers = [1, 2, 3];
+let user = [1, 'Andi'];
+const small = 1;
+const medium = 2;
+const large = 3;
+let newSize = 2;
+console.log(newSize);
+function calculateTax(income) {
+}
+function calculateTax2(income) {
+    return 0;
+}
+function calculateTax3(income) {
+    return 0;
+}
+function calculateTax4(income) {
+    if (income < 50000)
+        return income * 1.2;
+    return income * 1.3;
+    let x;
+}
+function calculateTax5(income, taxYear) {
+    if (taxYear < 2023)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax5(10000, 2022);
+function calculateTax6(income, taxYear) {
+    if ((taxYear || 2022) < 2023)
+        return income * 1.2;
+    return income * 1.3;
+}
+function calculateTax7(income, taxYear = 2022) {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+calculateTax7(10000);
+calculateTax7(10000, 2023);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ let age: number = 20;
 if (age > 50)
     age += 10;
 
-    // Primitives:
+    // <===PRIMITIVES===>
 
 let sales: number = 123_456_789;
 let sales2 = 321;
@@ -27,7 +27,7 @@ function render(document: any) {
     console.log(document)
 }
 
-// Arrays
+// <===ARRAYS===>
 let numbers = [1, 2, '3'];
 // string and numbers mixed, yet no error because valid in JS
 
@@ -37,3 +37,95 @@ let numbers = [1, 2, '3'];
 let trueNumbers: number[] = [1,2,3]
 // the : number[] is optional
 // empty array would turn it "any"
+// see syntax above if you wanna make sure the array can't be mixed
+
+// <===TUPLES===>
+
+// = fixed length array where each element has a perticular type, like id:number and name:string
+
+let user: [number, string] = [1, 'Andi'];
+
+// let user2: [number, string] = [1, 'Andi', 12];
+// doesn't work because the tuple is declared as number, string. If it was number, string, number, then it would work.
+
+// user.push(1);
+// possible, but problematic
+
+// KEEP TUPLES SHORT! JUST 2 VALUES!
+
+// <===ENUMS===>
+
+const small = 1;
+const medium = 2;
+const large = 3;
+
+const enum size{ small =1, medium, large }
+// you could also set values as you wish inside the {}
+
+let newSize: size = size.medium;
+console.log(newSize);
+
+// <===FUNCTIONS===>
+
+function calculateTax(income: number) {
+    
+}
+// void because no defined return value
+
+function calculateTax2(income: number) {
+    return 0;
+}
+// returns number now, but types need to be properly annotated
+
+function calculateTax3(income: number):number {
+    return 0;
+}
+
+// immediate compilation error when wrong values are to be returned; if you want it to return an empty vale, use :void instead of :number between the brackets
+
+// to ensure you don't forget never read values and implicit returns, go to tsconfig.json and activate 
+// "noUnusedParameters";true,
+// "noImplicitReturns":true,
+
+function calculateTax4(income: number):number {
+    if (income < 50_000)
+        return income * 1.2;
+    //without code below it would return undefined when >50_000; the else is optional here
+    return income * 1.3
+    
+    let x;
+    // no unused variables => set tsconfig "noUnusedLocals":true,
+}
+
+function calculateTax5(income: number, taxYear: number): number{
+    if (taxYear < 2023)
+        return income * 1.2;
+    return income * 1.3;
+}
+
+// now if you wanna call that function, you need exactly 2 parameters: income and taxYear
+//any extra parameters would throw compilation errors in TS but not in JS
+
+calculateTax5(10_000, 2022)
+
+// if you wanna make parameters optional, add a ? and adjust syntax in if()
+
+function calculateTax6(income: number, taxYear?: number): number{
+    if ((taxYear || 2022) < 2023)
+        return income * 1.2;
+    return income * 1.3;
+}
+
+// alternatively you could assign a fixed value. If you don't assign a value when calling the function, it'll automatically take the fixed, else it will overwrite it with the one you assigned on call
+
+function calculateTax7(income: number, taxYear=2022): number{
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+
+calculateTax7(10_000)
+// would automatically assign 2022 as the 2nd parameter; the double parenteses aren't needed anymore
+
+calculateTax7(10_000, 2023)
+// overwrites 2022 with 2023
